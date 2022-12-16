b38b2c5f0223a0bccdebfd6fe3e7419e8ee6831d
Faros <99401477+Faros998@users.noreply.github.com>
2022-12-16 14:19:53 +0100

    Update home.php

--- home.php
+++ home.php
@@ -14,7 +14,7 @@
 
 <button class="nav-item"><a role="button" onclick="darkmode()" title="Režim">Light | Dark</a></button>
 <button class="nav-item"><a role="button" onclick="window.location.href='index.php';" title="Logo">RtN</a></button>
-<button class="nav-item"><a role="button" title="Informace" onclick='alert("\nJak hrát Remember the Number\n\nTato hra procvičí tvé mozokvé závity. Princip je jednoduchý. Na krátkou dobu ti bude ukázáno číslo, které si musíš zapamatovat a následně napsat. Každá Session má 10 Levelů. Pak postupuješ do další Session, kde se ti budou zobrazovat již čísla delší. A tak to pokračuje dál.\n\nPS: Z důvodu testovaní je hra nastavena do Session 02 Level 10. Časový interval jsou 2s a délka náhodných čísel je nastavena v řádech desítek a stovek.");'>Info</a></button>
+<button class="nav-item"><a role="button" title="Informace" onclick='alert("\nJak hrát Remember the Number\n\nTato hra procvičí tvé mozkové závity. Princip je jednoduchý. Na krátkou dobu ti bude ukázáno číslo, které si musíš zapamatovat a následně napsat. Každá Session má 10 Levelů. Pak postupuješ do další Session, kde se ti budou zobrazovat již čísla delší. A tak to pokračuje dál.\n\nPS: Z důvodu testovaní je hra nastavena do Session 02 Level 10. Časový interval jsou 2s a délka náhodných čísel je nastavena v řádech desítek a stovek.");'>Info</a></button>
 
 <h1><center>Remember</center></h1>
 <h2><center>the</center></h2>
